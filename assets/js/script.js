2b7e4929bcac240920f5cd3bf2e9369d03715064
JimLynx <jim.lynx@gmail.com>
2020-08-23 16:31:37 +0100

    docs: Added smooth scrolling function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+//Credit: w3schools - smoothy scrolling
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+//END Credit: w3schools - smoothy scrolling
+
 // Close mobile dropdown menu after item clicked
 window.onclick = function (e) {
   var dropDown = document.getElementById("navbarResponsive");
