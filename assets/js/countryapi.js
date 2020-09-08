b2912ade432b8a2a256d5bbc4ade3f9a28efb9b3
JimLynx <jim.lynx@gmail.com>
2020-09-08 10:36:26 +0100

    Added country buttons from API info

---
+++ assets/js/countryapi.js
@@ -0,0 +1,13 @@
+//REST Country API from https://restcountries.eu/
+const API_URL = "https://restcountries.eu/rest/v2/alpha/";
+
+// Create country buttons
+axios.get(`${API_URL}?codes=zaf;bwa;nam;ken;`).then((res) => {
+  const container = document.querySelector(".btn-container");
+
+  for (i in res.data) {
+    let button = `<a class="button" type="button" data-country="${res.data[i]["alpha2Code"]}"><img src="${res.data[i]["flag"]}" alt="Flag of ${res.data[i]["name"]}" height=100 width=100/><span>${res.data[i]["name"]}</span></a>`;
+    // CREDIT: "insertAdjacentHTML & beforeend" parameters from Element.insertAdjacentHTML() - https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+    container.insertAdjacentHTML("beforeend", button);
+  }
+});
