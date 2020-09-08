0d91c08a1d08347cb1f7b58830b7e1f048b6dcf1
JimLynx <jim.lynx@gmail.com>
2020-09-08 16:34:00 +0100

    Fixed bug: buttons giving error on console

--- assets/js/countryapi.js
+++ assets/js/countryapi.js
@@ -2,54 +2,60 @@
 const API_URL = "https://restcountries.eu/rest/v2/alpha/";
 
 // Create country buttons
-axios.get(`${API_URL}?codes=zaf;bwa;nam;ken;`).then((res) => {
-  const container = document.querySelector(".btn-container");
+axios
+  .get(`${API_URL}?codes=zaf;bwa;nam;ken;`)
+  .then((res) => {
+    const container = document.querySelector(".btn-container");
 
-  for (i in res.data) {
-    let button = `<a class="button" type="button" data-country="${res.data[i]["alpha2Code"]}"><span>${res.data[i]["name"]}</span></a>`;
-    // CREDIT: "insertAdjacentHTML & beforeend" parameters from Element.insertAdjacentHTML() - https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
-    container.insertAdjacentHTML("beforeend", button);
-  }
-});
+    for (i in res.data) {
+      let button = `<div class="button" type="button" data-country="${res.data[i]["alpha2Code"]}">${res.data[i]["name"]}</div>`;
+      // CREDIT: "insertAdjacentHTML & beforeend" parameters from Element.insertAdjacentHTML() - https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+      container.insertAdjacentHTML("beforeend", button);
+    }
+  })
+  .then(() => {
+    const buttons = document.querySelectorAll(".button");
+    console.log("test", buttons);
+    for (i = 0; i < buttons.length; i++) {
+      // add a click event listener to the button
 
-const buttons = document.querySelectorAll(".button");
+      buttons[i].addEventListener("click", (event) => {
+        // send an api request that uses the 'data-country' data attr
+        axios
+          .get(`${API_URL}${event.target.dataset["country"]}`)
+          // Then when the data is recieved
+          .then((res) => {
+            // Name
+            document.querySelector(".countryheading").innerHTML =
+              res.data["name"];
 
-for (button in buttons) {
-  // add a click event listener to the button
-  this.addEventListener("click", () => {
-    // send an api request that uses the 'data-country' data attr
-    axios
-      .get(`${API_URL}${this.event.target.parentElement.dataset["country"]}`)
-      // Then when the data is recieved
-      .then((res) => {
-        // Name
-        document.querySelector(".countryheading").innerHTML = res.data["name"];
+            // Capital
+            document.querySelector(".capitaltext").innerHTML =
+              res.data["capital"];
 
-        // Capital
-        document.querySelector(".capitaltext").innerHTML = res.data["capital"];
+            // Population
 
-        // Population
+            //CREDIT: Refactored population number with Intl.NumberFormat() constructor - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
+            let population = `${Intl.NumberFormat().format(
+              res.data["population"]
+            )} (2018) World Bank`;
+            document.querySelector(".poptext").innerHTML = population;
 
-        //CREDIT: Refactored population number with Intl.NumberFormat() constructor - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
-        let population = `${Intl.NumberFormat().format(
-          res.data["population"]
-        )} (2018) World Bank`;
-        document.querySelector(".poptext").innerHTML = population;
+            // Languages
+            const langlist = document.querySelector(".langList");
+            langData = res.data["languages"];
+            langlist.innerHTML = "";
 
-        // Languages
-        const langlist = document.querySelector(".langList");
-        langData = res.data["languages"];
-        langlist.innerHTML = "";
+            for (language in langData) {
+              let language_item = `<li>${langData[language]["name"]}</li>`;
+              langlist.insertAdjacentHTML("beforeend", language_item);
+            }
 
-        for (language in langData) {
-          let language_item = `<li>${langData[language]["name"]}</li>`;
-          langlist.insertAdjacentHTML("beforeend", language_item);
-        }
-
-        // Flags
-        document
-          .querySelector(".container-image")
-          .setAttribute("src", res.data["flag"]);
+            // Flags
+            document
+              .querySelector(".container-image")
+              .setAttribute("src", res.data["flag"]);
+          });
       });
+    }
   });
-}
