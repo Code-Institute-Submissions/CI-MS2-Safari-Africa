f94be216bdc4c5c5d004c5570b4e71bcaf04d190
JimLynx <jim.lynx@gmail.com>
2020-09-08 12:16:00 +0100

    Removed flag import for display on buttons

--- assets/js/countryapi.js
+++ assets/js/countryapi.js
@@ -6,7 +6,7 @@ axios.get(`${API_URL}?codes=zaf;bwa;nam;ken;`).then((res) => {
   const container = document.querySelector(".btn-container");
 
   for (i in res.data) {
-    let button = `<a class="button" type="button" data-country="${res.data[i]["alpha2Code"]}"><img src="${res.data[i]["flag"]}" alt="Flag of ${res.data[i]["name"]}" height=100 width=100/><span>${res.data[i]["name"]}</span></a>`;
+    let button = `<a class="button" type="button" data-country="${res.data[i]["alpha2Code"]}"><span>${res.data[i]["name"]}</span></a>`;
     // CREDIT: "insertAdjacentHTML & beforeend" parameters from Element.insertAdjacentHTML() - https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
     container.insertAdjacentHTML("beforeend", button);
   }
