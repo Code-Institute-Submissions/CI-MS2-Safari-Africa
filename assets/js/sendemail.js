41e42345a575dca8d0f5a9cd45ceb1bbc3ac0b61
JimLynx <jim.lynx@gmail.com>
2020-09-15 15:03:38 +0100

    Added emailjs functionality to contact form

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("lynx", "ci-ms2", {
+        "from_firstname": contactForm.firstname.value,
+        "from_surname": contactForm.surname.value,
+        "from_email": contactForm.email.value,
+        "request_message": contactForm.message.value
+    })
+        .then(
+            function (response) {
+                alert("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error)
+            })
+    return false;
+}
