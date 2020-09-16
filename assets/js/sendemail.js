4dbac91639035b1892f42c2a5fd7bd7b490606f3
JimLynx <jim.lynx@gmail.com>
2020-09-16 17:25:17 +0100

    Added function to trigger email form success modal

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,16 +1,24 @@
+// Function adapted from CI tutorial for emailjs in mini-project walkthrough
 function sendMail(contactForm) {
     emailjs.send("lynx", "ci-ms2", {
         "from_firstname": contactForm.firstname.value,
-        "from_surname": contactForm.surname.value,
+        "from_lastname": contactForm.lastname.value,
         "from_email": contactForm.email.value,
         "request_message": contactForm.message.value
     })
         .then(
+            // Function to activate success modal
             function (response) {
-                alert("SUCCESS", response);
+                successModal();
             },
+            // On error, using standard browser alert
             function (error) {
-                console.log("FAILED", error)
+                alert("FAILED", error)
             })
     return false;
 }
+
+function successModal(e) {
+    $("#emailSuccess").modal("show");
+}
+
