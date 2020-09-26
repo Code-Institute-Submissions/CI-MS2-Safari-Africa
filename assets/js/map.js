28c96f52280bb9c210882d21bcc84491d8d9421e
JimLynx <jim.lynx@gmail.com>
2020-09-26 12:11:16 +0100

    Moved location of map-related code to map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,57 @@
+// Function to HIDE/SHOW country info with buttons
+const countryButton = document.getElementsByClassName("country-button");
+for (let i = 0; i < countryButton.length; i++) {
+  countryButton[i].addEventListener("click", handleCountrySelect);
+}
+function handleCountrySelect(e) {
+  const countryBtn = e.target.dataset.country;
+  const countrySelections1 = document.getElementsByClassName("country-section");
+  for (let i = 0; i < countrySelections1.length; i++) {
+    const sectionID = countrySelections1[i].id;
+    if (sectionID === countryBtn) {
+      countrySelections1[i].classList.remove("hidden");
+    } else {
+      countrySelections1[i].classList.add("hidden");
+    }
+  }
+  handleReserveReset("reserve-0");
+  // Click button to show map-country based on the innerText
+  changeMapLocation(e.target.innerText);
+}
+
+// Function to HIDE/SHOW RESERVE info with buttons
+const reserveButton = document.getElementsByClassName("reserve-button");
+for (let i = 0; i < reserveButton.length; i++) {
+  reserveButton[i].addEventListener("click", handleReserveSelect);
+}
+function handleReserveSelect(e) {
+  const reserveBtn = e.target.dataset.reserve;
+  const reserveSelection = document.getElementsByClassName("reserve-section");
+  for (let i = 0; i < reserveSelection.length; i++) {
+    const reserveID = reserveSelection[i].id;
+    if (reserveID === reserveBtn) {
+      reserveSelection[i].classList.remove("hidden");
+    } else {
+      reserveSelection[i].classList.add("hidden");
+    }
+  }
+  changeMapLocation(e.target.innerText); // click button to show map-country based on the innerText
+}
+
+// Function to RESET RESERVE info
+function handleReserveReset(reserve) {
+  const reserveBtn = reserve;
+  const reserveSelection = document.getElementsByClassName("reserve-section");
+  for (let i = 0; i < reserveSelection.length; i++) {
+    const reserveID = reserveSelection[i].id;
+    if (reserveID === reserveBtn) {
+      reserveSelection[i].classList.remove("hidden");
+    } else {
+      reserveSelection[i].classList.add("hidden");
+    }
+  }
+}
+
 //MAPS API from https://leafletjs.com/
 let mapTileLayers = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", //Attribution for map tiles import
