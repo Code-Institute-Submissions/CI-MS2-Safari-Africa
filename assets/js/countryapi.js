a7bc00e5b213a58c27931b13c21b486cce640296
JimLynx <jim.lynx@gmail.com>
2020-09-27 09:11:16 +0100

    Refactored to use dot notation for values

--- assets/js/countryapi.js
+++ assets/js/countryapi.js
@@ -8,10 +8,10 @@ axios
     const container = document.querySelector(".btn-container");
     let containerButtons = "";
     for (i in res.data) {
-      let button = `<div class="col-6 col-lg-3"><div class="api-button" type="button" data-country="${res.data[i]["alpha2Code"]}">${res.data[i]["name"]}</div></div>`;
+      let button = `<div class="col-6 col-lg-3"><div class="api-button" type="button" data-country="${res.data[i].alpha2Code}">${res.data[i].name}</div></div>`;
       containerButtons += button;
     }
-    container.innerHTML=`${containerButtons}`
+    container.innerHTML = `${containerButtons}`;
   })
   .then(() => {
     const buttons = document.querySelectorAll(".api-button");
@@ -22,35 +22,36 @@ axios
         axios
           .get(`${API_URL}${event.target.dataset["country"]}`)
           .then((res) => {            
-            document.querySelector(".countryHeading").innerHTML = res.data["name"]; // Country Name heading            
-            document.querySelector(".capitalText").innerHTML = res.data["capital"]; // Capital City
-            document.querySelector(".phoneText").innerHTML = `+${res.data["callingCodes"]}`; // Dialling Code
+            document.querySelector(".countryHeading").innerHTML = res.data.name; // Country Name heading            
+            document.querySelector(".capitalText").innerHTML = res.data.capital; // Capital City
+            document.querySelector(".phoneText").innerHTML = `+${res.data.callingCodes}`; // Dialling Code
 
             const localCurrency = document.querySelector(".currencyText"); // Currencies
-            currencyData = res.data["currencies"];
+            currencyData = res.data.currencies;
             localCurrency.innerHTML = "";
             for (currency in currencyData) {
-              let currency_item1 = `${currencyData[currency]["name"]}`;
-              let currency_item2 = `${currencyData[currency]["symbol"]}`;              
-              let currency_item3 = `${currencyData[currency]["code"]}`;
+              let currency_item1 = `${currencyData[currency].name}`;
+              let currency_item2 = `${currencyData[currency].symbol}`;              
+              let currency_item3 = `${currencyData[currency].code}`;
               localCurrency.insertAdjacentHTML("beforeend", `${currency_item1} (${currency_item2} or ${currency_item3})<br>`);
             }
 
-            document.querySelector(".timeText").innerHTML = res.data["timezones"]; // Int. Time Zone            
+            document.querySelector(".timeText").innerHTML = res.data.timezones; // Int. Time Zone            
             /*CREDIT: Refactored population number with Intl.NumberFormat() constructor
             https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat*/
-            let population = `${Intl.NumberFormat().format(res.data["population"])} (2018) World Bank`; // Population
+            let population = `${Intl.NumberFormat().format(res.data.population)} (2018) World Bank`; // Population
+            // End Credit
             document.querySelector(".populationText").innerHTML = population;
             
             const languageList = document.querySelector(".langList"); // Language List
-            langData = res.data["languages"];
+            langData = res.data.languages;
             languageList.innerHTML = "";
             for (language in langData) {
-              let language_item = `<li>${langData[language]["name"]}</li>`;
+              let language_item = `<li>${langData[language].name}</li>`;
               languageList.insertAdjacentHTML("beforeend", language_item);
             }
             
-            document.querySelector(".container-image").setAttribute("src", res.data["flag"]); // Flags
+            document.querySelector(".container-image").setAttribute("src", res.data.flag); // Flags
           });
       });
     }
